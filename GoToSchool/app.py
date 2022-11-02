119e2395c913e50c3eba43c1589035bff26fe8e7
Adaks <adakueli9@gmail.com>
2022-11-02 21:53:34 +0000

    Setup connection to the database

--- GoToSchool/app.py
+++ GoToSchool/app.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+from flask import Flask,jsonify,request
+from flask_cors import CORS
+
+
+from sqlalchemy import create_engine,MetaData
 
 # Route importations
 from Students.StudentService import students_route
@@ -11,9 +19,6 @@ from Classes.ClassService import classes_route
 
 app = Flask(__name__)
 
-
-
-
 # App Blueprinte
 app.register_blueprint(students_route)
 app.register_blueprint(rooms_route)
@@ -22,6 +27,27 @@ app.register_blueprint(lecturers_route)
 app.register_blueprint(courses_route)
 app.register_blueprint(classes_route)
 
+
+load_dotenv()
+
+engine = create_engine( os.getenv('dbconnectionstring'),pool_pre_ping = True,pool_size=20, max_overflow=10)#establish a connection with the database
+
+Session = sessionmaker(bind=engine)
+session = Session()
+meta = MetaData()
+from base import Base
+
+# Database Connection
+try:
+    engine.connect()
+    Base.metadata.create_all(engine)
+    session.commit()
+    print("Database Successfully Connected")
+except Exception as e:
+    print('Database connection failed: %s'%(e))
+    session.rollback()
+finally:
+    session.close()
 
 
 
