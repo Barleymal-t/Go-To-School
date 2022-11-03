a31b97cd80f37a5f99b9cee5ca073b4e159d271f
Adaks <adakueli9@gmail.com>
2022-11-03 14:58:36 +0000

    Update LessonService.py

--- GoToSchool/Lessons/LessonService.py
+++ GoToSchool/Lessons/LessonService.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime,date
 from random import randint
 from flask import request, jsonify, Blueprint
 # from werkzeug.security import generate_password_hash, check_password_hash
@@ -31,6 +31,7 @@ def getlessonss():
             Json_lessons.append({
                 "msg": {
 
+                "id_lesson": lesson.idLesson,
                 "id_lecturer": lecturer.idLecturer,
                 "course_name": course.courseName,
                 "class_name": classO.className,
@@ -50,41 +51,6 @@ def getlessonss():
             }
         }), 400
 
-
-# # Get all students
-# @students_route.route("/students", methods=['GET'])
-# def getStudents():
-#     from app import session
-#     try:
-#         lecturer = session.query(Lecturer).get(id)
-#         classO = session.query(Classi).get(id)
-#         room = session.query(Room).get(id)
-#         course = session.query(Course).get(id)
-
-
-#         students = session.query(Student).all()
-#         Json_students = [{
-
-#             "msg": {
-#                 "id_lecturer": student.idStudent,
-#                 "d_class": student.first_name,
-#                 "id_room": student.last_name,
-#                 "time_start": student.user_email,
-#                 "time_end": student.user_password,
-#                 "id_course": student.date_of_birth,
-#             },
-#             "status": True
-
-#         } for student in students]
-#         return jsonify(Json_students), 200
-#     except Exception as e:
-#         return ({
-#             'status': False,
-#             'msg': {
-#                 "dev_message": (f"{e}"),
-#                 "message": "Connection error: could not get students"
-#             }
-#         }), 400
 
 # Get lesson by ID
 @lessons_route.route("/lessons/<id>", methods=['GET'])
@@ -116,3 +82,106 @@ def getLessonById(id):
             }
         }), 400
 
+
+
+# delete patient
+@lessons_route.route("/lessons/<id>", methods=["DELETE"])
+def deleteLessonsById(id):
+    from app import session
+    try:
+
+        lesson = session.query(Lesson).get(id)
+
+        # delete patient with corresponding ID
+
+        session.delete(lesson)
+        session.commit()
+        return ({
+
+            "msg": {
+
+                "id_lecturer": lesson.idLecturer,
+                "id_class": lesson.idClass,
+                "id_room": lesson.idRoom,
+                "time_start": lesson.timeStart,
+                "time_end": lesson.timeEnd,
+                "id_course": lesson.idCourse,
+            },
+            "status": True
+
+        }), 200
+    except Exception as e:
+        session.rollback()
+        return ({
+            'status': False,
+            'msg': {
+                "dev_message": (f"{e}"),
+                "message": "Error: Could not delete patient"
+            }
+        }), 400
+
+
+# # Update patient info
+# @lessons_route.route("/lessons/<id>", methods=["PUT"])
+# def updateLessonDetailsById(id):
+#     from app import session
+#     req = request.json
+
+#     try:
+#         lesson = session.query(Lesson).get(id)
+
+#         # update details with new parameters
+#         lesson.idLecturer = req["id_lecturer"]
+#         lesson.idClass = req["id_class"]
+#         lesson.idRoom = req[""]
+#         lesson.timeStart = req[""]
+#         lesson.timeEnd = req[""]
+#         lesson.idCourse = req[""]
+
+#         patient.first_name = req["first_name"]
+#         patient.middle_name = req["middle_name"]
+#         patient.last_name = req["last_name"]
+#         patient.user_email = req["user_email"].lower()
+#         patient.person_image = req["person_image"]
+#         patient.date_of_birth = req["date_of_birth"]
+#         patient.house_address = req["house_address"]
+#         patient.phone_number = req["phone_number"]
+#         patient.id_number = req["id_number"]
+#         patient.nationality = req["nationality"]
+#         patient.gender = req["gender"]
+#         # patient.id_doctor = req["id_doctor"]
+#         patient.id_guardian = req["id_guardian"]
+
+#         session.commit()
+#         return ({
+
+#             "msg": {
+#                 "id_patient": patient.id_patient,
+#                 "first_name": patient.first_name,
+#                 "middle_name": patient.middle_name,
+#                 "last_name": patient.last_name,
+#                 "user_email": patient.user_email,
+#                 "person_image": patient.person_image,
+#                 "id_number": patient.id_number,
+#                 "id_guardian": patient.id_guardian,
+#                 "id_doctor": patient.id_doctor,
+#                 "house_address": patient.house_address,
+#                 "nationality": patient.nationality,
+#                 "phone_number": patient.phone_number,
+#                 "gender": patient.gender,
+#                 "date_of_birth": patient.date_of_birth,
+#                 "id_message": patient.id_message
+
+#             },
+#             "status": True
+
+#         }), 200
+#     except Exception as e:
+#         session.rollback()
+#         return ({
+#             'status': False,
+#             'msg': {
+#                 "dev_message": (f"{e}"),
+#                 "message": "Error: Could not update patient details"
+#             }
+#         }), 400
